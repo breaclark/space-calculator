f6a70baa02e05e00325f58c80f4b0dc6a20348ec
Brea Borlas <breatortilla@gmail.com>
2018-04-20 13:08:27 -0700

    Add button functionality to change planet image

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-// import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 //import { Person } from "./space.js";
+
+$(document).ready(function() {
+  $("#mercury-btn").click(function() {
+    $("#planet").attr("src","img/mercury.png");
+  });
+  $("#venus-btn").click(function() {
+    $("#planet").attr("src","img/venus.png");
+  });
+  $("#mars-btn").click(function() {
+    $("#planet").attr("src","img/mars.png");
+  });
+  $("#jupiter-btn").click(function() {
+    $("#planet").attr("src","img/jupiter.png");
+  });
+  $("#earth-btn").click(function() {
+    $("#planet").attr("src","img/earth.png");
+  });
+});
+
 
 
 // buttons should change the focal planet
