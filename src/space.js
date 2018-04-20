4843a30f42e815bafdb0c65b53cf2ec2f5e45150
Brea Borlas <breatortilla@gmail.com>
2018-04-20 11:06:52 -0700

    Add differenceInSeconds function just in case necessary

--- src/space.js
+++ src/space.js
@@ -1,13 +1,19 @@
-export function yearstoSeconds (input) {
+export function yearsToSeconds (input) {
   return input * 31556952;
+}
+
+export function differenceInSeconds (input1, input2) {
+  let date1 = new Date(input1);
+  let date2 = new Date(input2);
+  return (date2 - date1)/1000;
 }
 
 class Person {
   constructor (birthdate) {
     // needs to be in "year, month, day" format
     this.birthdate = new Date(birthdate);
-    this.now = new Date(Date.now());
-    //this.now = new Date(2018, 4, 20, 9, 42, 30, 0) // used for testing;
+    //this.now = new Date(Date.now());
+    this.now = new Date(2018, 4, 20, 9, 42, 30, 0) // used for testing;
     this.difference = this.now - this.birthdate;
   }
 
