54e406c266439ee528b47de4c5b4b849b765fd61
Brea Borlas <breatortilla@gmail.com>
2018-04-20 10:03:31 -0700

    Refactor to have difference between birthdate and now in seconds as property of Person class

--- spec/space-spec.js
+++ spec/space-spec.js
@@ -21,4 +21,5 @@ describe("Person", function () {
   });
 
 
+
 });
--- src/space.js
+++ src/space.js
@@ -2,14 +2,14 @@ class Person {
   constructor (birthdate) {
     //needs to be in "year, month, day" format
     this.birthdate = new Date(birthdate);
+    this.now = new Date(2018, 4, 20, 9, 42, 30, 0);
+    this.difference = this.now - this.birthdate;
   }
 
   birthdateToNowSeconds () {
-    let today = new Date(2018, 4, 20, 9, 42, 30, 0);
-    //this is in milliseconds
-    let difference = today - this.birthdate;
-    return difference/1000;
+    return this.difference/1000;
   }
+
 }
 
 export { Person };
