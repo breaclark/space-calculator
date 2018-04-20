3ded9886cc4335307ff2f801cdc8ed97d70ff4a6
Brea Borlas <breatortilla@gmail.com>
2018-04-20 11:53:55 -0700

    Add ids and comments

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 //import { Person } from "./space.js";
+
+
+// buttons should change the focal planet
+// should have an earth one as well
+// at the bottom have a tell me my age button
+// overlays on the planet your age in a pie chart of expected age
