3eb361014937d016946fa8ce3076545df2d0e66c
Brea Borlas <breatortilla@gmail.com>
2018-04-20 13:31:39 -0700

    Add age functionality for each of the planet buttons

--- src/main.js
+++ src/main.js
@@ -5,24 +5,40 @@ import "./styles.css";
 import { Person } from "./space.js";
 
 $(document).ready(function() {
+  let person;
   $("#mercury-btn").click(function() {
     $("#planet").attr("src","img/mercury.png");
+    if(person) {
+      $("#result").text(person.mercuryAge());
+    }
   });
   $("#venus-btn").click(function() {
     $("#planet").attr("src","img/venus.png");
+    if(person) {
+      $("#result").text(person.venusAge());
+    }
   });
   $("#mars-btn").click(function() {
     $("#planet").attr("src","img/mars.png");
+    if(person) {
+      $("#result").text(person.marsAge());
+    }
   });
   $("#jupiter-btn").click(function() {
     $("#planet").attr("src","img/jupiter.png");
+    if(person) {
+      $("#result").text(person.jupiterAge());
+    }
   });
   $("#earth-btn").click(function() {
     $("#planet").attr("src","img/earth.png");
+    if(person) {
+      $("#result").text(person.bithdate;
+    }
   });
 
   $("#bday-form").submit(function(event){
-    let person = new Person($("#bday").val());
+    person = new Person($("#bday").val());
     $("#result").text(person.birthdate);
     event.preventDefault();
   });
